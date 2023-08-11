0a5684c768371aac17b9e175a341cf398bc4117e
Ali Nawed <ali.nawed@mail.utoronto.ca>
2023-08-10 23:19:11 -0400

    added basic lighting

--- fragment.fs
+++ fragment.fs
@@ -1,7 +1,24 @@
 #version 330 core
 
+in vec3 norm; 
+
 out vec4 FragColor;
 
 void main() {
-	FragColor = vec4(1.0f,0.0f,0.0f, 1.0f);
+
+	float ambientStrength = 0.1;
+	vec3 lightColor = vec3(1.0,1.0,1.0);
+	vec3 objectColor = vec3(1.0, 0.0, 0.0);
+	vec3 lightDir = normalize(vec3(1.0, 1.0, 1.0));
+	
+	// ambient
+	vec3 ambient = ambientStrength * lightColor;
+	
+	// diffuse
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = diff * lightColor;
+	
+	vec3 result = (diffuse + ambient) * objectColor; 	
+
+	FragColor = vec4(result, 1.0f);
 }
--- main.cpp
+++ main.cpp
@@ -65,8 +65,11 @@ int main() {
         glBindBuffer(GL_ARRAY_BUFFER,VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(myVertices), myVertices, GL_STATIC_DRAW);
 
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
+	glEnableVertexAttribArray(1);
 
         glEnable(GL_DEPTH_TEST);
 
